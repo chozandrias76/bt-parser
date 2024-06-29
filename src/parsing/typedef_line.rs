e3d865cb91adb0c43c51845922206c394eea9e4c
Colin Swenson-Healey <chozandrias76@gmail.com>
2024-06-29 16:24:51 -0700

    feat: change typedef_line to ignore non typedef lines

--- src/parsing/typedef_line.rs
+++ src/parsing/typedef_line.rs
@@ -1,13 +1,11 @@
+use crate::types::nested::Nested;
 use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_until, take_while, take_while1},
-    character::complete::{char, multispace0},
-    combinator::recognize,
-    multi::many0_count,
-    sequence::{delimited, tuple},
+    bytes::complete::{tag, take_while},
+    character::complete::multispace0,
+    combinator::{opt, recognize},
+    sequence::tuple,
     IResult,
 };
-use crate::parsing::conditional_line::parse_conditional;
 
 fn parse_typedef_keyword(input: &str) -> IResult<&str, &str> {
     tag("typedef")(input)
@@ -21,35 +19,50 @@ fn parse_name(input: &str) -> IResult<&str, &str> {
     take_while(|c: char| c.is_alphanumeric() || c == '_')(input)
 }
 
-fn parse_braced_block(input: &str) -> IResult<&str, &str> {
-    recognize(tuple((
-        char('{'),
-        many0_count(alt((
-            delimited(char('{'), take_until("}"), char('}')),
-            take_while1(|c| c != '{' && c != '}'),
-            parse_conditional,
-        ))),
-        char('}'),
-    )))(input)
-}
-
-fn parse_remaining_block(input: &str) -> IResult<&str, &str> {
-    recognize(tuple((
-        parse_braced_block,
-        multispace0,
-        take_while(|c: char| c != ';'),
-        char(';'),
-    )))(input)
-}
-
-pub fn typedef_line(input: &str) -> IResult<&str, &str> {
-    recognize(tuple((
+pub fn typedef_line(input: &str) -> IResult<&str, Nested> {
+    let mut parser = tuple((
         parse_typedef_keyword,
         multispace0,
         parse_struct_keyword,
         multispace0,
-        parse_name,
-        multispace0,
-        parse_remaining_block,
-    )))(input)
+        opt(parse_name),
+    ));
+    parser(input).map(|(input, values)| {
+        let name = match values.4 {
+            Some(name) => name.into(),
+            None => "".into(),
+        };
+        let nested_texts = vec![
+            Nested::Text(values.0.into()),
+            // Ignore multi spaces
+            Nested::Text(values.2.into()),
+            // Ignore multi spaces
+            Nested::Text(name),
+        ];
+        (input, Nested::List(nested_texts))
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::nested::Nested;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_typedef_line1() {
+        let input = r#"typedef struct {
+} PlayerGameData <size=0x1B0>;
+"#;
+        let result = typedef_line(input);
+        assert_eq!(
+            result,
+            Ok((
+                r#"{
+} PlayerGameData <size=0x1B0>;
+"#,
+                vec!["typedef".into(), "struct".into(), "".into()].into()
+            ))
+        );
+    }
 }
