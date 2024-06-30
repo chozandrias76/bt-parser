1f7a7df2c17dacc7f58a8673b408b1c842a0195b
Colin Swenson-Healey <chozandrias76@gmail.com>
2024-06-29 19:35:54 -0700

    feat: slim down conditional_line to single responsibility

--- src/parsing/conditional_line.rs
+++ src/parsing/conditional_line.rs
@@ -1,89 +1,43 @@
 use nom::{
-  IResult,
-  bytes::complete::{tag, take_while1},
-  character::complete::{char, multispace0},
-  sequence::{delimited, preceded, tuple},
-  combinator::recognize,
-  multi::many0,
+    branch::alt,
+    bytes::complete::{tag, take_while1},
+    character::complete::{char, multispace0},
+    combinator::recognize,
+    error::context,
+    sequence::{preceded, tuple},
+    IResult,
 };
 
-fn parse_if_keyword(input: &str) -> IResult<&str, &str> {
-  tag("if")(input)
-}
-
-fn parse_else_if_keyword(input: &str) -> IResult<&str, &str> {
-  tag("else if")(input)
-}
-
-fn parse_condition(input: &str) -> IResult<&str, &str> {
-  delimited(char('('), take_while1(|c| c != ')'), char(')'))(input)
-}
-
-fn parse_braced_block(input: &str) -> IResult<&str, &str> {
-  let mut level = 0;
-  let mut i = 0;
-
-  for (j, c) in input.chars().enumerate() {
-      match c {
-          '{' => {
-              level += 1;
-          }
-          '}' => {
-              level -= 1;
-              if level == 0 {
-                  i = j;
-                  break;
-              }
-          }
-          _ => {}
-      }
-  }
-
-  if level == 0 {
-      Ok((&input[i + 1..], &input[..=i]))
-  } else {
-      Err(nom::Err::Error(nom::error::Error::new(input, nom::error::ErrorKind::Char)))
-  }
-}
-
-fn parse_if_block(input: &str) -> IResult<&str, &str> {
-  recognize(tuple((
-      parse_if_keyword,
-      multispace0,
-      parse_condition,
-      multispace0,
-      parse_braced_block,
-  )))(input)
-}
-
-fn parse_else_if_block(input: &str) -> IResult<&str, &str> {
-  recognize(tuple((
-      parse_else_if_keyword,
-      multispace0,
-      parse_condition,
-      multispace0,
-      parse_braced_block,
-  )))(input)
-}
-
-fn parse_if_else_blocks(input: &str) -> IResult<&str, &str> {
-  recognize(tuple((
-      parse_if_block,
-      many0(preceded(multispace0, parse_else_if_block)),
-  )))(input)
-}
-
 pub fn conditional_line(input: &str) -> IResult<&str, &str> {
-  parse_if_else_blocks(input)
+    let mut parser = context(
+        "conditional_line",
+        recognize(tuple((
+            alt((tag("if"), tag("else if"))),
+            preceded(multispace0, char('(')),
+            take_while1(|c: char| c != '{'),
+        ))),
+    );
+    parser(input).map(|(input, s)| (input, s.trim_end()))
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
+    use pretty_assertions::assert_eq;
 
-  #[test]
-  fn test_parse_conditional() {
-      let input_if_else = r#"if ( (ItemID != 0) && ((ItemID & 0xf0000000) == 0)) { 
+    #[test]
+    fn test_parse_conditional1() {
+        let input_if_else = r#"if ( (ItemID != 0) && ((ItemID & 0xf0000000) == 0)) { 
+          int32 unk; 
+          int32 unk2;
+          int32 ash_of_war_ga_item_handle<format=hex>;
+          byte unk4;
+      }
+      else if((ItemID != 0) && ((ItemID & 0xf0000000) == 0x10000000)) { 
+          int32 unk;
+          int32 unk2;
+      }"#;
+        let expected_rest = r#"{ 
           int32 unk; 
           int32 unk2;
           int32 ash_of_war_ga_item_handle<format=hex>;
@@ -94,6 +48,32 @@ mod tests {
           int32 unk2;
       }"#;
 
-      assert_eq!(conditional_line(input_if_else), Ok(("", input_if_else)));
-  }
+        assert_eq!(
+            conditional_line(input_if_else),
+            Ok((
+                expected_rest,
+                "if ( (ItemID != 0) && ((ItemID & 0xf0000000) == 0))"
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_conditional2() {
+        let input_if_else = r#"else if((ItemID != 0) && ((ItemID & 0xf0000000) == 0x10000000)) {
+          int32 unk;
+          int32 unk2;
+      }"#;
+        let expected_rest = r#"{
+          int32 unk;
+          int32 unk2;
+      }"#;
+
+        assert_eq!(
+            conditional_line(input_if_else),
+            Ok((
+                expected_rest,
+                "else if((ItemID != 0) && ((ItemID & 0xf0000000) == 0x10000000))"
+            ))
+        );
+    }
 }
